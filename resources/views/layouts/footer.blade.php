a9dceb78890b1d415f0656fc198560272482c5de
shotout <hevaldo66@gmail.com>
2022-12-12 07:44:44 +0700

    Adjust Layout

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -6,16 +6,18 @@
                 <a style="color: #fff;" href="{{url('/privacy')}}">Privacy Policy</a>
             </div>
             <div class="col-md-6 c-footer-right">
-                <a href="https://instagram.com/mooti.app" target="_blank" rel="noopener noreferrer">
+                <a href="https://instagram.com/mooti.app" target="_blank" style="text-decoration: none;" >
                     <img class="mr-2" src="{{url('/assets/images/instagram.png')}}" alt="instagram logo">
                 </a>
-                <a href="https://facebook.com/MootiApp" target="_blank" rel="noopener noreferrer">
+                <a href="https://facebook.com/MootiApp" target="_blank" rel="noopener noreferrer" style="text-decoration: none;">
                     <img class="mr-2" src="{{url('/assets/images/facebook.png')}}" alt="facebook logo">
                 </a>
-                <a href="https://twitter.com/MootiApp" target="_blank" rel="noopener noreferrer">
+                <a href="https://twitter.com/MootiApp" target="_blank" rel="noopener noreferrer" style="text-decoration: none;">
                     <img class="mr-2" src="{{url('/assets/images/twitter.png')}}" alt="twitter logo">
                 </a>
+                <a href="https://mooti.app" target="_blank" style="color: #fff; text-decoration: none;" rel="noopener noreferrer">
                 | <span class="ml-2">©2022 Mooti</span>
+                </a>
             </div>
         </div>
     </div>
